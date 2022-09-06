addacc6ef80d4f945f3a31e05c0b2f65aa0bd59d
Yvan Sraka <yvan@sraka.xyz>
2022-09-06 10:08:09 +0200

    Add `aarch64-darwin` to supported target systems list

--- flake.nix
+++ flake.nix
@@ -4,7 +4,11 @@
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
   inputs.flake-utils.url = github:numtide/flake-utils;
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachSystem [
+    # List of supported target systems:
+    "x86_64-linux"
+    "aarch64-darwin"
+  ] (system:
     with import nixpkgs { inherit system; };
     rec {
       packages.nixprof = python3Packages.buildPythonApplication {
